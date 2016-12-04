752f4b1fb8802b6b8f591220244ada023fca8db2
Catherine Crooke <Cat@Catherines-MacBook-Pro.local>
2016-12-03 18:48:36 -0600

    updated .js file so that time for next train and minutes away updates with real time

--- assets/javascript/train.js
+++ assets/javascript/train.js
@@ -1,6 +1,7 @@
 // initialize the javascript document 
 $(document).ready(function() {
 
+    // function renderData(time, ) {
     // Initialize Firebase
     var config = {
         apiKey: "AIzaSyBA9aMvPQ6pxeyEC4nS2WPR9rR2FxhEqYI",
@@ -34,9 +35,8 @@ $(document).ready(function() {
         hour = $('#first-train-time-hour');
         minute = $('#first-train-time-minute');
         firstTrain = moment(hour.val().trim() + ":" + minute.val().trim(), 'HH:mm').subtract(1, 'years').format('X');
-        timeDifference = moment().diff(moment.unix(firstTrain), 'minutes');
-        minutesAway = frequency - (timeDifference % frequency);
-        nextTrain = moment().add(minutesAway, 'minutes').format('HH:mm');
+
+
 
         //Push the values to the keys in the database
         // Code for handling the push
@@ -45,8 +45,9 @@ $(document).ready(function() {
             destination: destination,
             frequency: frequency,
             startTime: firstTrain,
-            nextArrival: nextTrain,
-            minutesAway: minutesAway,
+            // nextArrival: nextTrain,
+            // minutesAway: minutesAway,
+
 
         });
 
@@ -60,16 +61,43 @@ $(document).ready(function() {
         return false;
     });
 
+    function render() {
+        var rows = $('<div>');
+        for (var i = 0; i < trains.length; i++) {
+            var train = trains[i];
+            var row = $('<tr>');
+            $(row).append($('<td>').text(train.trainName));
+            $(row).append($('<td>').text(train.destination));
+            $(row).append($('<td>').text(train.frequency));
+            // $(row).append($('<td>').text(train.nextArrival));
+            // $(row).append($('<td>').text(train.minutesAway));
+
+
+            var timeDifference = moment().diff(moment.unix(train.startTime), 'minutes');
+            // console.log('difference : ' + timeDifference);
+            var minutesAway = train.frequency - (timeDifference % train.frequency);
+            // console.log('minutes : ' + minutesAway);
+
+            var nextTrain = moment().add(minutesAway, 'minutes').format('HH:mm');
+            $(row).append($('<td>').text(nextTrain));
+            $(row).append($('<td>').text(minutesAway));
+            rows.append(row);
+
+        }
+        $('#table-body').empty().append(rows.children());
+
+    }
+
+    setInterval(render, 1000 * 60);
+
+    var trains = [];
+
     // // Firebase watcher + initial loader HINT: This code behaves similarly to .on("value")
     database.ref().on('child_added', function(childSnapshot) {
+            var childValue = childSnapshot.val();
+            trains.push(childValue);
+            render();
 
-            var row = $('<tr>');
-            $(row).append($('<td>').text(childSnapshot.val().trainName));
-            $(row).append($('<td>').text(childSnapshot.val().destination));
-            $(row).append($('<td>').text(childSnapshot.val().frequency));
-            $(row).append($('<td>').text(childSnapshot.val().nextArrival));
-            $(row).append($('<td>').text(childSnapshot.val().minutesAway));
-            $('#table-body').append(row);
 
 
             // Handle the errors
@@ -78,4 +106,6 @@ $(document).ready(function() {
             console.log('Errors handled: ' + errorObject.code);
 
         });
+    // }
+
 });
